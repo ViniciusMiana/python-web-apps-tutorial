77c18aaec6bca7ed6acce03006b20954a01853f7
Jader H. Silva <jaderhs5@gmail.com>
2013-07-14 18:19:59 -0300

    Handle add/remove inside model

--- example/app/app/models.py
+++ example/app/app/models.py
@@ -5,7 +5,7 @@ class Stock():
     """Model for webservices stock data"""
 
     def list_stocks(self):
-
+        """List all stocks from webserice"""
         conn = HTTPConnection("localhost", 8080)
         conn.request("GET", "/stock/")
         response = conn.getresponse()
@@ -14,3 +14,20 @@ class Stock():
         jsobj = json.loads(js)
 
         return jsobj["stocks"]
+
+    def add_stock(self, stock):
+        """Add a stock to webservice"""
+        conn = HTTPConnection("localhost", 8080)
+        conn.request("POST", "/stock/{0}".format(stock))
+        response = conn.getresponse()
+
+        if response.status != 200:
+            raise Exception(response.read())
+        return True
+
+
+    def remove_stock(self, stock):
+        """Remove a stock from webservice"""
+        conn = HTTPConnection("localhost", 8080)
+        conn.request("DELETE", "/stock/{0}".format(stock))
+        response = conn.getresponse()
